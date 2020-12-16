4670fe25b491be37ccae4239e4646a6d741758c2
José Eduardo Brandão <zefutbrandao@gmail.com>
2020-12-16 16:48:21 -0300

    por enquanto CAS! ou CAO!

---
+++ trab1/1.c
@@ -0,0 +1,80 @@
+/* Programa para mostrar sincronizacao de threads. O programa deveria
+ *  imprimir "SOP", mas isso nem sempre ocorre.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define down(x) sem_wait(x)
+#define up(x) sem_post(x)
+
+sem_t sA, sB, sC;
+
+
+
+void *a(void *argp)
+{
+    while(1) {
+      down(&sC);
+      printf("O");
+      up(&sB);
+    }
+     pthread_exit(NULL);
+}
+
+void *b(void *argp)
+{
+  
+    while(1) {
+
+      printf("CA");
+      up(&sC);
+      down(&sB);
+      printf("!\n");
+    }
+    // up(&sB);
+
+     pthread_exit(NULL);
+}
+
+void *c(void *argp)
+{
+   while(1) {
+      down(&sC);
+      printf("S");
+      up(&sB);
+    }
+     pthread_exit(NULL);
+}
+
+/* P2 espera P1 
+
+semaphore S = 0;
+
+P1:          P2: 
+  executa      down(&S)
+  up(&S);      executa 
+*/
+
+int main(void)
+{
+     pthread_t t1, t2, t3;
+     int rc;
+
+     sem_init(&sA, 0, 0);     
+     sem_init(&sB, 0, 0);
+     sem_init(&sC, 0, 0);
+     rc = pthread_create(&t1, NULL, a, NULL);   assert(rc == 0);
+     rc = pthread_create(&t2, NULL, b, NULL);   assert(rc == 0);
+     rc = pthread_create(&t3, NULL, c, NULL);   assert(rc == 0);
+     rc = pthread_join(t1, NULL);   assert(rc == 0);
+     rc = pthread_join(t2, NULL);   assert(rc == 0);
+     rc = pthread_join(t3, NULL);   assert(rc == 0);
+     putchar('\n');
+     return 0;
+}
